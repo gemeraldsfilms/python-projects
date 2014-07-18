21cadd69951a6b13908737d8fc94fd97b5c495fd
gemeraldsfilms <joshua.reynolds511@gmail.com>
2014-07-18 16:33:41 -0400

    This is getting even MORE boring
    dont read this

---
+++ May own.py
@@ -0,0 +1,11 @@
+name = input ("What the heck is your name and what are you doing here?!?! ")
+getout = input ("Get out of here " + name)
+
+print = input("I SAID GET OUT " + name)
+wellthatsgreat = input ("well that's just great now i have " +name+ " in my house")
+
+print = input("Why did you say " +wellthatsgreat  +  name)
+really = input ("really")
+print = input("are you sure?")
+great = input ("Great!")
+print = input("I'm happy now!!")
